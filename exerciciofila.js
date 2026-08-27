d8fc243216fd351457bf77c1591228199df01e3c
vlosopedro <vlosopedro10@gmail.com>
2026-08-26 21:32:28 -0300

    atualização

---
+++ exerciciofila.js
@@ -0,0 +1,13 @@
+let queue = [];  
+ queue.push("jesca");
+ queue.push("joao");
+ queue.push("deiveso");
+ queue.push("jorge");
+ queue.push("cleberso");
+
+   console.log(queue);
+
+ while ( queue.Lenght >= 0 );{
+    atendido = queue.shift();
+    console.log(atendido);
+ }
